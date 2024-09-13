bd8fc2c0d30d212261df980518754be9d4717859
Selwyn Orren <selwyn@linuxweb.co.za>
2024-09-13 10:41:16 +0200

    Update FA_Repacker.sh
    Fixed an issue with the json not compiling properly

--- FA_Repacker.sh
+++ FA_Repacker.sh
@@ -1,90 +1,105 @@
 #!/bin/bash
 
-# FA_Repacker.sh
-Output=""
-Clean=false
+# Define parameters
+output=""
+clean=false
 
-while [[ $# -gt 0 ]]; do
+# Parse arguments
+while [[ "$#" -gt 0 ]]; do
     case $1 in
-        -o|--output)
-            Output="$2"
-            shift
-            shift
-            ;;
-        -c|--clean)
-            Clean=true
-            shift
-            ;;
-        *)
-            shift
-            ;;
+        --output) output="$2"; shift ;;
+        --clean) clean=true ;;
     esac
+    shift
 done
 
-Path=$(pwd)
-if [[ -z "$Output" ]]; then
-    Output="$Path"
+# Set path variables
+path=$(pwd)
+if [ -z "$output" ]; then
+    output="$path"
 fi
 
-startTime=$(date)
+start_time=$(date)
+cd "$path"
 
-areas=("Assets") # Uncomment if needed
-#areas=("Assets" "Textures_Natural" "Textures_Artificial" "Legacy" "Roof_Patterns" "Roofs")
+# Uncomment to process only "Legacy"
+#areas=("Legacy")
+areas=("Assets" "Textures_Natural" "Textures_Artificial" "Legacy" "Roof_Patterns" "Roofs")
 
-for currentFocus in "${areas[@]}"; do
-    packPrefix="FA_${currentFocus}_REPACK_"
-    packName="${packPrefix}$(date +%m-%d-%y)"
-    joinedPath="$Path/$currentFocus/joined"
+for current_focus in "${areas[@]}"; do
+    # Uncomment the line below to manually set the current focus
+    # current_focus="Textures_Artificial"
 
-    for file in *"$currentFocus"*; do
-        if [[ -f "$file" ]]; then
-            ./dungeondraft-unpack -O -vv "$file" "$currentFocus/unpacked"
-        fi
+    pack_prefix="FA_${current_focus}_REPACK_"
+    # Uncomment to use MM-dd-yy format for pack name
+    # pack_name="${pack_prefix}$(date +%m-%d-%y)"
+    pack_name="${pack_prefix}$(date +%Y)"
+    joined_path="${path}/${current_focus}/joined"
+
+    # Create necessary directories
+    mkdir -p "$joined_path"
+    mkdir -p "${current_focus}/unpacked"
+
+    # Unpack files using dungeondraft-unpack
+    find . -maxdepth 1 -type f -name "*$current_focus*" -print0 | while IFS= read -r -d '' file; do
+        ./dungeondraft-unpack -O -vv "$file" "${current_focus}/unpacked"
     done
 
-    cd "$currentFocus" || exit
-    mkdir -p 'joined'
+    # Navigate to the current focus directory
+    cd "$current_focus"
+    mkdir -p "joined"
 
-    cd "unpacked" || exit
-    firstDir=$(ls -d */ | head -n 1 | cut -f1 -d'/')
-    cd "$firstDir" || exit
+    # Process the unpacked directory
+    cd "unpacked"
+    first_dir=$(find . -maxdepth 1 -type d | sed -n '2p')
+    cd "$first_dir"
 
-    manifest=$(cat 'pack.json')
-    # Uncomment and modify the following lines if needed
-    # manifest=$(echo "$manifest" | jq --arg name "$packName" --arg version "$(date +%m%d%y)" --arg id "FA$manifest" '.name=$name | .version=$version | .id=$id')
-    # echo "$manifest" | jq . > "$joinedPath/pack.json"
+    # Read and manipulate JSON manifest
+    manifest_file="pack.json"
+    manifest=$(jq '.' "$manifest_file")
+    
+    # Uncomment and modify the lines below if manifest fields need to be updated
+    # manifest=$(echo "$manifest" | jq ".name=\"$pack_name\"")
+    # manifest=$(echo "$manifest" | jq ".version=\"$(date +%m%d%y)\"")
+    # manifest_id="FA$manifest"
+    # manifest=$(echo "$manifest" | jq ".id=\"$manifest_id\"")
+    echo "$manifest" | jq '.' > "$joined_path/pack.json"
 
-    mv "preview.png" "$joinedPath" 2>/dev/null
+    # Move preview.png to the joined directory
+    find . -name "preview.png" -exec mv {} "$joined_path" \;
 
-    cd .. || exit
+    cd ..
 
-    for dir in */; do
-        ogName="${dir%/}"
-        cd "$ogName" || exit
-        for subDir in */; do
-            sourcePath="$Path/$currentFocus/unpacked/$ogName/$subDir"
-            destinationFolder="$joinedPath"
-
-            # Move files from sourcePath to destinationFolder
-            mv "$sourcePath"/* "$destinationFolder"/ 2>/dev/null
+    # Move content from unpacked subdirectories to joined directory
+    find . -maxdepth 1 -mindepth 1 -type d | while IFS= read -r subdir; do
+        cd "$subdir"
+        for folder in */; do
+            source_path="${path}/${current_focus}/unpacked/${subdir}/${folder}"
+            cp -r "$source_path"/* "$joined_path/"
         done
-        cd .. || exit
+        cd ..
     done
 
-    cd .. || exit
-
+    cd ..
+    
+    # Remove unpacked directory
     rm -rf "unpacked"
 
-    cd .. || exit
-    # Remove older repacks:
-    rm -f "$packPrefix"* 2>/dev/null
-    ./dungeondraft-pack -O -E -A "$manifest.author" -N "$packName" -V "$(date +%m%d%y)" "$joinedPath" "$Output"
+    cd ..
+    
+    # Remove older repacks
+    find . -maxdepth 1 -type f -name "${pack_prefix}*" -exec rm -f {} +
 
-    rm -rf "$currentFocus" 2>/dev/null
-    if $Clean; then
-        rm -f *"$currentFocus"* 2>/dev/null
+    # Repack using dungeondraft-pack
+    author=$(echo "$manifest" | jq -r '.author')
+    ./dungeondraft-pack -O -E -A "$author" -N "$pack_name" -V "$(date +%m%d%y)" "$joined_path" "$output"
+    
+    # Clean up directories
+    rm -rf "$current_focus"
+    if [ "$clean" = true ]; then
+        find . -maxdepth 1 -type f -name "*$current_focus*" -exec rm -f {} +
     fi
 
-    echo "Start Time: $startTime"
+    echo "Start Time: $start_time"
     echo "End Time: $(date)"
 done
